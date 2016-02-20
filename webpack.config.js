8d830d76c5452700faf6d19485b07ba33054fb86
Royce Tucker <roytucke@gmail.com>
2016-02-20 12:34:09 -0600

    Copying resource files to the output directory Used for favicon/save to homescreen stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 var CleanWebpackPlugin = require('clean-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-var outputPath = require('path').resolve(__dirname, 'www');
+var path = require('path');
+var outputPath = path.resolve(__dirname, 'www');
 
 module.exports = {
     entry: "./src/app.js",
@@ -12,7 +14,7 @@ module.exports = {
     },
     module: {
         loaders: [
-            {test: /\.html$/, loader: 'html-loader?minimize=false'},
+            {test: /\.html$/, loader: 'html-loader'},
             {test: /\.(gif|jpg|png|svg|woff|woff2)$/, loader: 'file-loader'},
             {test: /\.handlebars$/, loader: 'handlebars-loader'},
             {
@@ -23,6 +25,10 @@ module.exports = {
     },
     plugins: [
         new CleanWebpackPlugin([outputPath]),
+        new CopyWebpackPlugin([
+            {from: 'src/resources', to: 'resources'},
+            {from: 'src/manifest.json'}
+        ]),
         new ExtractTextPlugin('styles.[hash].css', {allChunks: true}),
         new HtmlWebpackPlugin({template: 'src/index.html', inject: 'head'})
     ],
