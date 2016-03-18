1aa224d43d3592f72500dd9a8b83c0a65f154253
Royce Tucker <roytucke@gmail.com>
2016-03-18 14:42:35 -0500

    Removing project images

--- src/components/projectListItem.js
+++ src/components/projectListItem.js
@@ -1,12 +1,7 @@
 var renderTemplate = require('../core/renderTemplate');
 var template = require('../templates/projectItem.handlebars');
-var defaultProjectImage = require('../images/default.png');
 
 module.exports = function (project) {
-    if (!project.image) {
-        project.image = defaultProjectImage;
-    }
-
     this.project = project;
     this.element = renderTemplate(template(project));
     this.element.addEventListener('click', function () {
