40a43d01d0da611427fdb106bdb0bfd23cb95fa7
Royce Tucker <roytucke@gmail.com>
2016-04-03 18:41:03 -0500

    Moving back to a simple home screen

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,29 +4,8 @@ var renderTemplate = require('../core/renderTemplate');
 var template = require('../templates/home.html');
 
 function HomePage(header) {
-    this.header = header;
+    header.update();
+    this.element = renderTemplate(template);
 }
-
-HomePage.prototype.onRoute = function () {
-    return connection.getProjectsForUser().then(function (projects) {
-        this.header.update({title: 'My Projects'});
-        this.element = renderTemplate(template);
-
-        if (projects && projects.length) {
-            var fragment = document.createDocumentFragment();
-            for (var i = 0; i < projects.length; i++) {
-                var project = projects[i];
-                if (project) {
-                    fragment.appendChild(new ProjectListItem(project).element);
-                }
-            }
-
-            this.element.querySelector('.project-list').appendChild(fragment);
-        }
-        else {
-            this.element.querySelector('.button--create-project').hidden = false;
-        }
-    }.bind(this));
-};
 
 module.exports = HomePage;
