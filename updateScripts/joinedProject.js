eb1f2c480eb20f0cd862935fd78cc5fd5fe707a6
Royce Tucker <roytucke@gmail.com>
2016-05-31 19:06:10 -0500

    Adding an upgrade script

---
+++ updateScripts/joinedProject.js
@@ -0,0 +1,22 @@
+// This upgrades user.projects from a value of `true` to a value of `{joined: true}`
+
+import Firebase from '../src/firebase/firebase';
+
+export default function upgrade() {
+    var ref = Firebase.get().child('users');
+    ref.on('value', function (snapshot) {
+        var users = snapshot.val();
+
+        for (var userId in users) {
+            var user = users[userId];
+            for (var projectId in user.projects) {
+                var project = user.projects[projectId];
+                if (project === true) {
+                    user.projects[projectId] = {joined: true};
+                }
+            }
+        }
+
+        ref.set(users);
+    });
+}
